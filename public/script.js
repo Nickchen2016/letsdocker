b2e83407bc98349af5393333b3befa1293d5970f
Nick Chen <chenyahua2012@hotmail.com>
2020-07-09 00:25:37 -0400

    detect background img avg darkness

--- public/script.js
+++ public/script.js
@@ -1,15 +1,31 @@
-const img = document.getElementById('back-img');
-const img_src = img.src;
+function checkrgb(){
+    const img = document.getElementById('back-img');
+    let colorSum = 0;
 
-window.onload = () => {
-        console.log('hello', img_src)
-
+    // create canvas
     const canvas = document.createElement('canvas');
-    canvas.width = '200'; canvas.height = '200';
+    canvas.width = '100'; canvas.height = '100';
 
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(img, canvas.width, canvas.height);
-    const imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-    const data = imageData.data;
-    console.log('_____', data)
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(img,0,0);
+
+    let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+    let data = imageData.data;
+    let r,g,b,avg;
+
+    for(let x = 0, len = data.length; x < len; x+=4) {
+        r = data[x];
+        g = data[x+1];
+        b = data[x+2];
+
+        avg = Math.floor((r+g+b)/3);
+        colorSum += avg;
+    }
+
+    let brightness = Math.floor(colorSum / (canvas.width*canvas.height));
+    document.getElementById('font-color').innerHTML = brightness;
 }
+
+document.getElementById('font-color').addEventListener('click',()=>{
+    checkrgb();
+})
